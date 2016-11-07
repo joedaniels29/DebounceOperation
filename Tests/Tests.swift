1844649f555f349c32e6c3e8704afb9d052865ac
Joseph Daniels <joedaniels29@me.com>
2016-11-07 11:58:21 -0500

    Initial commit

---
+++ Tests/Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Tests.swift
+//  DebounceOperation
+//
+//  Created by Károly Lőrentey on 2016-03-08.
+//  Copyright © 2016 Károly Lőrentey.
+//
+
+import XCTest
+@testable import DebounceOperation
+
+class DebounceOperationTests: XCTestCase {
+    
+    func testHello() {
+        XCTAssertEqual(hello(), "Hello!")
+    }
+}
